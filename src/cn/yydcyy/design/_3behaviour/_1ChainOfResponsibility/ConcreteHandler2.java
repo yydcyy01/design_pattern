c26dd053b649924127e2397e6341a834547b0afe
yuyang <447395536@qq.com>
2019-11-24 10:06:59 +0800

    单例模式 - 枚举实现 单例模式 (Effective Java 作者推荐做法

--- src/cn/yydcyy/design/_3behaviour/_1ChainOfResponsibility/ConcreteHandler2.java
+++ src/cn/yydcyy/design/_3behaviour/_1ChainOfResponsibility/ConcreteHandler2.java
@@ -4,9 +4,9 @@ package cn.yydcyy.design._3behaviour._1ChainOfResponsibility;
  * @author YYDCYY
  * @create 2019-11-24
  */
-public class ConcreteHandler1 extends Handler{
+public class ConcreteHandler2 extends Handler{
     //构造器
-    public ConcreteHandler1(Handler successor){
+    public ConcreteHandler2(Handler successor){
         super(successor);
     }
 
@@ -15,13 +15,14 @@ public class ConcreteHandler1 extends Handler{
     protected void handleRequest(Request request) {
 
         // 枚举类型判断
-        if (request.getType() == RequestType.TYPE1){
-            System.out.println(request.getName() + " is handle by ConcreteHandler1 ");
+        if (request.getType() == RequestType.TYPE2){
+            System.out.println(request.getName() + " is handle by ConcreteHandler2 ");
             return;
         }
 
         if (successor != null){
             successor.handleRequest(request);
         }
+        //不是上面类型且为空, 就不处理呗.
     }
 }
--- src/cn/yydcyy/design/_3behaviour/_1ChainOfResponsibility/Handler.java
+++ src/cn/yydcyy/design/_3behaviour/_1ChainOfResponsibility/Handler.java
@@ -11,5 +11,9 @@ public abstract class Handler {
         this.successor = successor;
     }
 
+    /**
+     * 业务逻辑 : 对象连成一条链, 沿着链发送请求, 知道有一个对象处理它为止
+     * @param request
+     */
     protected abstract  void handleRequest(Request request);
 }
